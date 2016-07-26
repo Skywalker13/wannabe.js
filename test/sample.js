453b98fe50eedb042de1ef8e6075ffd127b014a2
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-07-26 15:34:15 +0200

    Change a bit the sample
    In order to be less optimzable.

--- test/sample.js
+++ test/sample.js
@@ -1,8 +1,16 @@
 'use strict';
 
+function get (v) {
+  switch (v)
+  {
+  case 1: return 'foo';
+  case 2: return 'bar';
+  }
+}
+
 describe ('a', function () {
   it ('a.1', function () {
-    const test = "blupi" + "a";
-    const a1 = "toto";
+    let test = get (1);
+    let a1 = test + ' - ' + get (2);
   });
 });
