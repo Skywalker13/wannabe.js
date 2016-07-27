59e22f275d03a970c4a5630774f6e08b6bb9f299
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-07-27 14:13:34 +0200

    Fix async mocha test

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,20 +6,27 @@ const wannabe  = require ('../lib/index.js');
 
 
 describe ('index', function () {
-  it ('#default', function () {
+  it ('#default', function (done) {
     wannabe (path.join (__dirname, './sample.js'), 'it', 'a.1')
       .then ((frames, err) => {
         expect (err).to.be.undefined;
         expect (frames).to.be.eql ({
-          '12': {
-            name: 'foo',
+          '12': [{
+            name: 'test',
+            value: 'foo'
+          }, {
+            name: 'a1',
             value: undefined
-          },
-          '13': {
-            name: 'foo',
+          }],
+          '13': [{
+            name: 'test',
+            value: 'foo'
+          }, {
+            name: 'a1',
             value: 'foo - bar'
-          }
+          }]
         });
+        done ();
       });
   });
 });
