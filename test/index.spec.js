b18960ee27c52a49bdd592fd0adf40ee1dcd49e7
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-07-28 16:22:27 +0200

    Cosmetic: fix indent level

--- test/index.spec.js
+++ test/index.spec.js
@@ -8,25 +8,25 @@ const wannabe  = require ('../lib/index.js');
 describe ('index', function () {
   it ('#default (good js)', function (done) {
     wannabe (path.join (__dirname, './sample.js'), null, 'it', 'a.1', (err, frames) => {
-        expect (err).to.be.null;
-        expect (frames).to.be.eql ({
-          '12': [{
-            name: 'test',
-            value: 'foo'
-          }, {
-            name: 'a1',
-            value: undefined
-          }],
-          '13': [{
-            name: 'test',
-            value: 'foo'
-          }, {
-            name: 'a1',
-            value: 'foo - bar'
-          }]
-        });
-        done ();
+      expect (err).to.be.null;
+      expect (frames).to.be.eql ({
+        '12': [{
+          name: 'test',
+          value: 'foo'
+        }, {
+          name: 'a1',
+          value: undefined
+        }],
+        '13': [{
+          name: 'test',
+          value: 'foo'
+        }, {
+          name: 'a1',
+          value: 'foo - bar'
+        }]
       });
+      done ();
+    });
   });
 
   it ('#default (bad js)', function (done) {
