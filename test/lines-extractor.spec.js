06a0226617c0235fc108efaebab26a331ea76c5b
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-08-11 11:14:19 +0200

    Cosmetic: rename linesEx to LinesEx

--- test/lines-extractor.spec.js
+++ test/lines-extractor.spec.js
@@ -2,7 +2,7 @@
 
 const path           = require ('path');
 const {expect}       = require ('chai');
-const linesExtractor = require ('../lib/lines-extractor.js');
+const LinesExtractor = require ('../lib/lines-extractor.js');
 
 
 const script = `/*  1 */
@@ -34,22 +34,22 @@ const script = `/*  1 */
 
 describe ('lines-extractor', function () {
   it ('#byFile (regex)', function () {
-    expect (linesExtractor.byFile (path.join (__dirname, './sample.js'), 'it', /a.[0-9]/))
+    expect (LinesExtractor.byFile (path.join (__dirname, './sample.js'), 'it', /a.[0-9]/))
       .to.be.eql ([12, 13]);
   });
 
   it ('#byFile (string)', function () {
-    expect (linesExtractor.byFile (path.join (__dirname, './sample.js'), 'it', 'a.1'))
+    expect (LinesExtractor.byFile (path.join (__dirname, './sample.js'), 'it', 'a.1'))
       .to.be.eql ([12, 13]);
   });
 
   it ('#byData (regex)', function () {
-    expect (linesExtractor.byData (script, 'it', /^_.*/))
+    expect (LinesExtractor.byData (script, 'it', /^_.*/))
       .to.be.eql ([6, 7, 9, 10, 12, 16, 17, 19, 20, 22]);
   });
 
   it ('#byData (string)', function () {
-    expect (linesExtractor.byData (script, 'it', '_foo'))
+    expect (LinesExtractor.byData (script, 'it', '_foo'))
       .to.be.eql ([6, 7, 9, 10, 12]);
   });
 });
